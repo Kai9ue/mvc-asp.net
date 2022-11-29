df673c5c41099ffbbcabb28d6ee810818eafcd56
Kaique Carvalho <102710926+Kai9ue@users.noreply.github.com>
2022-11-28 23:07:50 -0300

    creating dark mode

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,7 @@
-﻿
+﻿const btn = document.querySelector('.btn')
+const darkContainer = document.querySelector('.darkContainer')
+
+btn.onclick = function () {
+  this.classList.toggle('active')
+  darkContainer.classList.toggle('active')
+}
